6545333ce5c334893cd96342d550b20d887f6336
Md Zubair Beg <ziibii88@gmail.com>
2023-09-05 14:22:45 +0800

    expose schema views to public

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,17 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    # redirect root path to swagger schema view
+    path("", RedirectView.as_view(pattern_name="swagger"), name="root"),
     path("admin/", admin.site.urls),
+    # include app urls
     path("", include("core.urls")),
     path("", include("proxy.urls")),
+    # drf-spectacular schema views
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
-
-if settings.DEBUG:  # expose schema views only in debug mode
-    urlpatterns.extend(
-        [
-            path("schema/", SpectacularAPIView.as_view(), name="schema"),  # type: ignore
-            path("schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),  # type: ignore
-            path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),  # type: ignore
-        ]
-    )
